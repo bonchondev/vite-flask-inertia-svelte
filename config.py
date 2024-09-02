5f22a894b60d62af324a13dcede7cb34401d13ba
bonchondev <benzay212@gmail.com>
2024-09-02 00:09:23 -0400

    create app to config

--- config.py
+++ config.py
@@ -1,4 +1,8 @@
 from pathlib import Path
+from flask import Flask
+from flask_inertia import Inertia
+from flask_vite import Vite
+
 
 ROOT_DIR = Path(".")
 
@@ -7,3 +11,23 @@ TEMPLATES = Path(".") / "templates"
 STATIC_FOLDER = Path(".") / "static"
 
 PUBLIC_FOLDER = Path(".") / "public"
+
+
+def create_app() -> Flask:
+
+    app = Flask(
+        __name__,
+        template_folder=TEMPLATES,
+        static_folder=STATIC_FOLDER,
+    )
+
+    app.config["INERTIA_TEMPLATE"] = "base.html"
+    app.config["SECRET_KEY"] = "changeme"
+    app.config["VITE_AUTO_INSERT"] = app.debug
+    app.config["VITE_NPM_BIN_PATH"] = "pnpm"
+
+    Inertia(app)
+
+    Vite(app)
+
+    return app
